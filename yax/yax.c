0cc4a9feb14e3a83168049432c4e9b682ba3edbe
yukihundo <155275392+yukihundo@users.noreply.github.com>
2023-12-31 13:01:20 +0900

    Add files via upload

---
+++ yax/yax.c
@@ -0,0 +1,11 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+int main()
+{
+    int a = 0,x=0,y=0;
+    printf("傾きaは?");scanf("%d",&a);
+    for (x = -10; x <= 10; x++) {
+        y = a * x;
+        printf("x=%d,y=%d\r\n",x,y);
+    }
+}
